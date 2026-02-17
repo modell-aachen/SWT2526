b1c019c12086fbfd7c140d9aa8dc65e9cda45675
Samuel S. <118532412+megustaamigo@users.noreply.github.com>
2026-02-17 15:57:13 +0100

    feat: dragging lines can be toggled off by pressing shift + drag

--- src/types/ElementWrapperEvents.ts
+++ src/types/ElementWrapperEvents.ts
@@ -4,7 +4,7 @@ export type ElementWrapperEvents = {
   (event: 'select', e: MouseEvent): void
   (event: 'click', e: MouseEvent): void
   (event: 'drag-start', e: MouseEvent): void
-  (event: 'drag', deltaX: number, deltaY: number): void
+  (event: 'drag', deltaX: number, deltaY: number, mouseEvent: MouseEvent): void
   (event: 'drag-end'): void
   (event: 'resize-start', position: string, mouseEvent: MouseEvent): void
   (event: 'resize', handle: ResizeHandle, deltaX: number, deltaY: number): void
